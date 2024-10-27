041d3ae89c35f8320aca01dccdd0d81bfa600285
multifacs <fedorets20@gmail.com>
2024-10-27 18:13:50 +0300

    initial tbb

---
+++ tasks/tbb/fedorets_i_simpson/func_tests/main.cpp
@@ -0,0 +1,346 @@
+// Copyright 2024 Fedorets Ilia
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "tbb/fedorets_i_simpson/include/simpson.hpp"
+
+TEST(fedorets_i_simpson_tbb, Func_1) {
+  // Create data
+  std::vector<double> out(1, 0);
+
+  const std::function<double(std::vector<double>)> f = [](std::vector<double> vec) {
+    double x = vec[0];
+    double y = vec[1];
+    return x * x - 2 * y;
+  };
+
+  std::vector<std::pair<double, double>> limits({{4, 10}, {1, 2}});
+  std::vector<int> n({3, 3});
+
+  auto funcPtr = std::make_unique<std::function<double(std::vector<double>)>>(f);
+  auto limPtr = std::make_unique<std::vector<std::pair<double, double>>>(limits);
+  auto nPtr = std::make_unique<std::vector<int>>(n);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonSequential taskSequential(taskDataSeq);
+  ASSERT_EQ(taskSequential.validation(), true);
+  taskSequential.pre_processing();
+  std::chrono::steady_clock::time_point begin_seq = std::chrono::steady_clock::now();
+  taskSequential.run();
+  std::chrono::steady_clock::time_point end_seq = std::chrono::steady_clock::now();
+  taskSequential.post_processing();
+  ASSERT_NEAR(294, out[0], 0.1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+
+  taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataParallel->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonParallel taskParallel(taskDataParallel);
+  ASSERT_EQ(taskParallel.validation(), true);
+  taskParallel.pre_processing();
+  std::chrono::steady_clock::time_point begin_par = std::chrono::steady_clock::now();
+  taskParallel.run();
+  std::chrono::steady_clock::time_point end_par = std::chrono::steady_clock::now();
+  taskParallel.post_processing();
+  ASSERT_NEAR(294, out[0], 0.1);
+
+  std::cout << "Seq    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_seq - begin_seq).count() << std::endl;
+  std::cout << "TBB    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_par - begin_par).count() << std::endl;
+}
+
+TEST(fedorets_i_simpson_tbb, Func_2) {
+  // Create data
+  std::vector<double> out(1, 0);
+
+  const std::function<double(std::vector<double>)> f = [](std::vector<double> vec) {
+    double x = vec[0];
+    double y = vec[1];
+    double z = vec[2];
+    return log10(2 * x * x) + sqrt(z) + 5 * y;
+  };
+
+  std::vector<std::pair<double, double>> limits({{4, 10}, {1, 2}, {2, 5}});
+  std::vector<int> n({2, 2, 2});
+
+  auto funcPtr = std::make_unique<std::function<double(std::vector<double>)>>(f);
+  auto limPtr = std::make_unique<std::vector<std::pair<double, double>>>(limits);
+  auto nPtr = std::make_unique<std::vector<int>>(n);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonSequential taskSequential(taskDataSeq);
+  ASSERT_EQ(taskSequential.validation(), true);
+  taskSequential.pre_processing();
+  std::chrono::steady_clock::time_point begin_seq = std::chrono::steady_clock::now();
+  taskSequential.run();
+  std::chrono::steady_clock::time_point end_seq = std::chrono::steady_clock::now();
+  taskSequential.post_processing();
+  ASSERT_NEAR(203.7, out[0], 0.1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+
+  taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataParallel->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonParallel taskParallel(taskDataParallel);
+  ASSERT_EQ(taskParallel.validation(), true);
+  taskParallel.pre_processing();
+  std::chrono::steady_clock::time_point begin_par = std::chrono::steady_clock::now();
+  taskParallel.run();
+  std::chrono::steady_clock::time_point end_par = std::chrono::steady_clock::now();
+  taskParallel.post_processing();
+  ASSERT_NEAR(203.7, out[0], 0.1);
+
+  std::cout << "Seq    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_seq - begin_seq).count() << std::endl;
+  std::cout << "TBB    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_par - begin_par).count() << std::endl;
+}
+
+TEST(fedorets_i_simpson_tbb, Func_3) {
+  // Create data
+  std::vector<double> out(1, 0);
+
+  const std::function<double(std::vector<double>)> f = [](std::vector<double> vec) {
+    double x = vec[0];
+    double y = vec[1];
+    double z = vec[2];
+    return x * y * z;
+  };
+
+  std::vector<std::pair<double, double>> limits({{4, 10}, {1, 2}, {4, 5}});
+  std::vector<int> n({2, 2, 2});
+
+  auto funcPtr = std::make_unique<std::function<double(std::vector<double>)>>(f);
+  auto limPtr = std::make_unique<std::vector<std::pair<double, double>>>(limits);
+  auto nPtr = std::make_unique<std::vector<int>>(n);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonSequential taskSequential(taskDataSeq);
+  ASSERT_EQ(taskSequential.validation(), true);
+  taskSequential.pre_processing();
+  std::chrono::steady_clock::time_point begin_seq = std::chrono::steady_clock::now();
+  taskSequential.run();
+  std::chrono::steady_clock::time_point end_seq = std::chrono::steady_clock::now();
+  taskSequential.post_processing();
+  ASSERT_NEAR(283.5, out[0], 0.1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+
+  taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataParallel->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonParallel taskParallel(taskDataParallel);
+  ASSERT_EQ(taskParallel.validation(), true);
+  taskParallel.pre_processing();
+  std::chrono::steady_clock::time_point begin_par = std::chrono::steady_clock::now();
+  taskParallel.run();
+  std::chrono::steady_clock::time_point end_par = std::chrono::steady_clock::now();
+  taskParallel.post_processing();
+  ASSERT_NEAR(283.5, out[0], 0.1);
+
+  std::cout << "Seq    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_seq - begin_seq).count() << std::endl;
+  std::cout << "TBB    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_par - begin_par).count() << std::endl;
+}
+
+TEST(fedorets_i_simpson_tbb, Func_4) {
+  // Create data
+  std::vector<double> out(1, 0);
+
+  const std::function<double(std::vector<double>)> f = [](std::vector<double> vec) {
+    double x = vec[0];
+    double y = vec[1];
+    double z = vec[2];
+    return exp(x) - sqrt(10) * 5 * sin(y) + cos(-2 * z * z);
+  };
+
+  std::vector<std::pair<double, double>> limits({{4, 10}, {1, 2}, {0, 5}});
+  std::vector<int> n({2, 2, 2});
+
+  auto funcPtr = std::make_unique<std::function<double(std::vector<double>)>>(f);
+  auto limPtr = std::make_unique<std::vector<std::pair<double, double>>>(limits);
+  auto nPtr = std::make_unique<std::vector<int>>(n);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonSequential taskSequential(taskDataSeq);
+  ASSERT_EQ(taskSequential.validation(), true);
+  taskSequential.pre_processing();
+  std::chrono::steady_clock::time_point begin_seq = std::chrono::steady_clock::now();
+  taskSequential.run();
+  std::chrono::steady_clock::time_point end_seq = std::chrono::steady_clock::now();
+  taskSequential.post_processing();
+  ASSERT_NEAR(111816.7, out[0], 0.1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+
+  taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataParallel->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonParallel taskParallel(taskDataParallel);
+  ASSERT_EQ(taskParallel.validation(), true);
+  taskParallel.pre_processing();
+  std::chrono::steady_clock::time_point begin_par = std::chrono::steady_clock::now();
+  taskParallel.run();
+  std::chrono::steady_clock::time_point end_par = std::chrono::steady_clock::now();
+  taskParallel.post_processing();
+  ASSERT_NEAR(111816.7, out[0], 0.1);
+
+  std::cout << "Seq    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_seq - begin_seq).count() << std::endl;
+  std::cout << "TBB    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_par - begin_par).count() << std::endl;
+}
+
+TEST(fedorets_i_simpson_tbb, Func_5) {
+  // Create data
+  std::vector<double> out(1, 0);
+
+  const std::function<double(std::vector<double>)> f = [](std::vector<double> vec) {
+    double x = vec[0];
+    double y = vec[1];
+    double z = vec[2];
+    return exp(x) + sqrt(2) * sin(y * z) + cos(z * z);
+  };
+
+  std::vector<std::pair<double, double>> limits({{1, 10}, {1, 3}, {0, 6}});
+  std::vector<int> n({2, 2, 2});
+
+  auto funcPtr = std::make_unique<std::function<double(std::vector<double>)>>(f);
+  auto limPtr = std::make_unique<std::vector<std::pair<double, double>>>(limits);
+  auto nPtr = std::make_unique<std::vector<int>>(n);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataSeq->inputs_count.emplace_back(1);
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonSequential taskSequential(taskDataSeq);
+  ASSERT_EQ(taskSequential.validation(), true);
+  taskSequential.pre_processing();
+  std::chrono::steady_clock::time_point begin_seq = std::chrono::steady_clock::now();
+  taskSequential.run();
+  std::chrono::steady_clock::time_point end_seq = std::chrono::steady_clock::now();
+  taskSequential.post_processing();
+  ASSERT_NEAR(287147.7, out[0], 0.1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataParallel = std::make_shared<ppc::core::TaskData>();
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(funcPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(limPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+  taskDataParallel->inputs.emplace_back(reinterpret_cast<uint8_t *>(nPtr.get()));
+  taskDataParallel->inputs_count.emplace_back(1);
+
+  taskDataParallel->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataParallel->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  fedorets_i_simpson_tbb::SimpsonParallel taskParallel(taskDataParallel);
+  ASSERT_EQ(taskParallel.validation(), true);
+  taskParallel.pre_processing();
+  std::chrono::steady_clock::time_point begin_par = std::chrono::steady_clock::now();
+  taskParallel.run();
+  std::chrono::steady_clock::time_point end_par = std::chrono::steady_clock::now();
+  taskParallel.post_processing();
+  ASSERT_NEAR(287147.7, out[0], 0.1);
+
+  std::cout << "Seq    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_seq - begin_seq).count() << std::endl;
+  std::cout << "TBB    time [µs]: "
+            << std::chrono::duration_cast<std::chrono::microseconds>(end_par - begin_par).count() << std::endl;
+}
---
+++ tasks/tbb/fedorets_i_simpson/src/simpson.cpp
@@ -0,0 +1,157 @@
+// Copyright 2024 Fedorets Ilia
+
+#include "tbb/fedorets_i_simpson/include/simpson.hpp"
+
+#include <thread>
+
+using namespace std::chrono_literals;
+
+double fedorets_i_simpson_tbb::getSimpson(std::function<double(std::vector<double>)>* f,
+                                          std::vector<std::pair<double, double>>* limits, std::vector<int>* n) {
+  int dim = (*n).size();
+  std::vector<double> h(dim);
+  int64_t counter = 1;
+  for (int i = 0; i < dim; ++i) {
+    h[i] = ((*limits)[i].second - (*limits)[i].first) / (*n)[i];
+    counter *= (*n)[i];
+  }
+  double result = 0.0;
+  for (int i = 0; i < counter; ++i) {
+    std::vector<std::vector<double>> params(dim);
+    int temp = i;
+    for (int j = 0; j < dim; ++j) {
+      params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j]);
+      params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+      params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+      params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+      params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+      params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j]);
+      temp /= (*n)[j];
+    }
+    std::vector<double> point;
+    for (int ii = 0; ii < pow(6, dim); ++ii) {
+      int temp = ii;
+      for (int j = 0; j < dim; ++j) {
+        point.push_back(params[j][temp % 6]);
+        temp /= 6;
+      }
+      result += (*f)(point);
+      point.clear();
+    }
+    params.clear();
+  }
+  for (int i = 0; i < dim; ++i) {
+    result *= h[i] / 6.0;
+  }
+  return result;
+}
+
+double fedorets_i_simpson_tbb::getTBBSimpson(std::function<double(std::vector<double>)>* f,
+                                             std::vector<std::pair<double, double>>* limits, std::vector<int>* n) {
+  int dim = n->size();
+  std::vector<double> h(dim);
+  int64_t counter = 1;
+  for (int i = 0; i < dim; ++i) {
+    h[i] = ((*limits)[i].second - (*limits)[i].first) / (*n)[i];
+    counter *= (*n)[i];
+  }
+
+  double result = tbb::parallel_reduce(
+      tbb::blocked_range<int>(0, counter), 0.0,
+      [&](tbb::blocked_range<int> r, double local_result) {
+        for (int i = r.begin(); i < r.end(); ++i) {
+          std::vector<std::vector<double>> params(dim);
+          int temp = i;
+          for (int j = 0; j < dim; ++j) {
+            params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j]);
+            params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+            params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+            params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+            params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j] / 2);
+            params[j].push_back((*limits)[j].first + temp % (*n)[j] * h[j] + h[j]);
+            temp /= (*n)[j];
+          }
+          std::vector<double> point;
+          for (int i = 0; i < pow(6, dim); ++i) {
+            int temp = i;
+            for (int j = 0; j < dim; ++j) {
+              point.push_back(params[j][temp % 6]);
+              temp /= 6;
+            }
+            local_result += (*f)(point);
+            point.clear();
+          }
+          params.clear();
+        }
+
+        return local_result;
+      },
+      std::plus<double>());
+
+  for (int i = 0; i < dim; ++i) {
+    result *= h[i] / 6.0;
+  }
+  return result;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonSequential::pre_processing() {
+  internal_order_test();
+  // Init value for input and output
+  // input_ = reinterpret_cast<int*>(taskData->inputs[0])[0];
+  f = reinterpret_cast<std::function<double(std::vector<double>)>*>(taskData->inputs[0]);
+  limits = reinterpret_cast<std::vector<std::pair<double, double>>*>(taskData->inputs[1]);
+  n = reinterpret_cast<std::vector<int>*>(taskData->inputs[2]);
+
+  res = 0;
+  return true;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonSequential::validation() {
+  internal_order_test();
+  // Check count elements of output
+  // return taskData->inputs_count[0] == 1 && taskData->outputs_count[0] == 1;
+  return taskData->inputs_count.size() == 3 && taskData->outputs_count.size() == 1;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonSequential::run() {
+  internal_order_test();
+  res = getSimpson(f, limits, n);
+  return true;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonSequential::post_processing() {
+  internal_order_test();
+  reinterpret_cast<double*>(taskData->outputs[0])[0] = res;
+  return true;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonParallel::pre_processing() {
+  internal_order_test();
+  // Init value for input and output
+  // input_ = reinterpret_cast<int*>(taskData->inputs[0])[0];
+  f = reinterpret_cast<std::function<double(std::vector<double>)>*>(taskData->inputs[0]);
+  limits = reinterpret_cast<std::vector<std::pair<double, double>>*>(taskData->inputs[1]);
+  n = reinterpret_cast<std::vector<int>*>(taskData->inputs[2]);
+
+  res = 0;
+  return true;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonParallel::validation() {
+  internal_order_test();
+  // Check count elements of output
+  // return taskData->inputs_count[0] == 1 && taskData->outputs_count[0] == 1;
+  return taskData->inputs_count.size() == 3 && taskData->outputs_count.size() == 1;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonParallel::run() {
+  internal_order_test();
+  res = getTBBSimpson(f, limits, n);
+  return true;
+}
+
+bool fedorets_i_simpson_tbb::SimpsonParallel::post_processing() {
+  internal_order_test();
+  reinterpret_cast<double*>(taskData->outputs[0])[0] = res;
+  return true;
+}
